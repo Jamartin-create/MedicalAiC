6bf2e0a7b8c733796dc2d8831a8d2f8a5ab8aa89
LilMartin <martin_du@qq.com>
2024-03-18 00:10:48 +0800

    🎈 perf(flexable): 顶部响应式布局优化

--- src/layout/Container/NavTopBar.vue
+++ src/layout/Container/NavTopBar.vue
@@ -6,17 +6,18 @@
             <div class="w-auto h-auto d-flex">
                 <template v-for="tab in items" :key="tab.name">
                     <div
-                        class="cursor-pointer px-10 py-4 rounded-pill text-body-2 font-weight-regular"
+                        class="cursor-pointer px-6 py-4 rounded-pill text-body-2 font-weight-regular"
                         :class="{ 'preset-bg-dark text-white': tab.active }"
                         @click="handleChangeTab(tab.value)"
                     >
-                        {{ tab.label }}
+                        <v-icon class="mdi mx-2" :class="tab.meta.icon" />
+                        <span class="label">{{ tab.label }}</span>
                     </div>
                 </template>
             </div>
         </v-container>
         <template v-slot:prepend>
-            <v-sheet class="preset-bg d-flex align-center">
+            <v-sheet class="prepend preset-bg d-flex align-center">
                 <div class="logo-wrp mx-4">
                     <img class="w-100" :src="logo" />
                 </div>
@@ -25,7 +26,7 @@
         </template>
         <template v-slot:append>
             <v-sheet
-                class="preset-bg-light w-auto mr-5 px-1 py-1 rounded-pill d-flex align-center justify-center"
+                class="append preset-bg-light w-auto mr-5 px-1 py-1 rounded-pill d-flex align-center justify-center"
             >
                 <Settings />
                 <v-btn class="text-body-2 mx-2" size="small" icon="mdi-help" />
@@ -48,12 +49,28 @@ type TabT = {
     label: string
     path: string
     value: string
+    icon: string
 }
 
 const tabs: TabT[] = [
-    { label: '工作台', path: '/dashboard/home', value: 'Home' },
-    { label: '你的助手', path: '/dashboard/chat/create', value: 'Chat' },
-    { label: '健康档案', path: '/dashboard/case/list', value: 'CaseList' }
+    {
+        label: '工作台',
+        path: '/dashboard/home',
+        value: 'Home',
+        icon: 'mdi-square-rounded-badge-outline '
+    },
+    {
+        label: '你的助手',
+        path: '/dashboard/chat/create',
+        value: 'Chat',
+        icon: 'mdi-robot-love-outline'
+    },
+    {
+        label: '健康档案',
+        path: '/dashboard/case/list',
+        value: 'CaseList',
+        icon: 'mdi-folder-file-outline'
+    }
 ]
 
 // 利用 hook 获取需要的列表 & 一些公用事件
@@ -68,5 +85,17 @@ const { items, handleChangeTab } = useCreateTabs<TabT>(tabs, {
 <style scoped>
 .logo-wrp {
     width: 50px;
+}
+
+@media screen and (max-width: 800px) {
+    span.label {
+        display: none;
+    }
+}
+@media screen and (max-width: 700px) {
+    .prepend,
+    .append {
+        display: none !important;
+    }
 }
 </style>
